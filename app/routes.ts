0913778f81c11ac83221108706c1d6db24b29769
heythere-itsme <mayankkumar.current@gmail.com>
2025-05-27 09:47:04 +0530

    Check

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,12 @@
 import { type RouteConfig, layout, route } from "@react-router/dev/routes";
+import { createBrowserRouter } from "react-router";
 
 export default [
     route('/', 'routes/visitor/home.tsx'),
     route('/car', 'routes/visitor/carShow.tsx'),
     route('/form', 'routes/form/form.tsx'),
 ] satisfies RouteConfig;
+
+createBrowserRouter([
+    
+])
