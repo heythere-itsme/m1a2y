923cf475b8e5f59b6f8245957165bd3d85896574
Mayank_Kumar <mayankkumar.current@gmail.com>
2025-05-28 09:51:30 +0530

    Testing Push

--- app/routes.ts
+++ app/routes.ts
@@ -4,9 +4,7 @@ import { createBrowserRouter } from "react-router";
 export default [
     route('/', 'routes/visitor/home.tsx'),
     route('/car', 'routes/visitor/carShow.tsx'),
-    route('/form', 'routes/form/form.tsx'),
+    layout('routes/form/form.tsx', [
+        route('/form/1', 'components/Form1.tsx')
+    ])
 ] satisfies RouteConfig;
-
-createBrowserRouter([
-    
-])
